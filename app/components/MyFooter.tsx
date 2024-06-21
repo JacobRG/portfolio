ea42e7595796505ac045f49c90a3fbab6a2e7df9
JacobRG <jacobgambrell@gmail.com>
2024-06-20 23:14:57 -0400

    Updated metadata

--- app/components/MyFooter.tsx
+++ app/components/MyFooter.tsx
@@ -56,7 +56,7 @@ const navigation = [
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-gray-500">
-              &copy; 2024 By Jacob Gambrell. All rights reserved.
+              &copy; 2024 Jacob Gambrell. All rights reserved.
             </p>
           </div>
         </div>
